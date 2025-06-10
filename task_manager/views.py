6ab4c47206f6683534f113b801c1d2b50a7c27dc
karanice <to.anathema@gmail.com>
2025-06-10 21:49:51 +0300

    Add view and urlpattern for start page

---
+++ task_manager/views.py
@@ -0,0 +1,18 @@
+# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.contrib import messages
+
+
+class IndexView(View):
+    template_name = "index.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "base.html",
+            context={
+            },
+        )
